52a266778c2ced28484df0db3aa4db77e8a662e6
José Juan Reyes Zuñiga <juan@makingdevs.com>
2023-02-13 15:19:26 -0600

    Use conn for response

--- lib/traefik/handler.ex
+++ lib/traefik/handler.ex
@@ -16,18 +16,19 @@ defmodule Traefik.Handler do
     %{method: method, path: path, response: ""}
   end
 
-  def route(_conn) do
-    %{method: "GET", path: "/hello", response: "Hello world"}
+  def route(conn) do
+    %{conn | response: "Hello world!😘"}
   end
 
-  def format_response(_conn) do
+  def format_response(conn) do
     """
     HTTP/1.1 200 OK
     Host: some.com
     User-Agent: telnet
+    Content-Lenght: #{String.length(conn.response)}
     Accept: */*
 
-    Hello world
+    #{conn.response}
     """
   end
 end
