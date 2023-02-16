21b6fd534b2023b357620f84e30fccb4d08c4478
José Juan Reyes Zuñiga <juan@makingdevs.com>
2023-02-16 15:36:51 -0600

    Won't use temp variables

--- lib/traefik/organization.ex
+++ lib/traefik/organization.ex
@@ -3,13 +3,11 @@ defmodule Traefik.Organization do
   @devs_file Path.expand("./") |> Path.join("MOCK_DATA.csv")
 
   def list_developers do
-    {:ok, data} =
-      @devs_file
-      |> File.read()
-
-    # [_ | _devs_text] =
-    data
+    @devs_file
+    |> File.read()
+    |> elem(1)
     |> String.split("\n")
+    |> Kernel.tl()
     |> Enum.map(fn e -> String.split(e, ",") end)
     |> Enum.map(fn e -> transform_developer(e) end)
   end
