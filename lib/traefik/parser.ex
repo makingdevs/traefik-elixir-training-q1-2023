d919dc71a95329cd93eeee65db7c9f7d373b4489
José Juan Reyes Zuñiga <juan@makingdevs.com>
2023-02-16 14:50:50 -0600

    PArse headers from request

--- lib/traefik/parser.ex
+++ lib/traefik/parser.ex
@@ -4,12 +4,28 @@ defmodule Traefik.Parser do
   def parse(request) do
     [main, params_string] = String.split(request, "\n\n")
 
-    [request_line | _headers] = String.split(main, "\n")
+    [request_line | headers_string] = String.split(main, "\n")
 
     [method, path, _protocol] = String.split(request_line, " ")
 
     params = URI.decode_query(params_string)
 
-    %Conn{method: method, path: path, params: params}
+    headers = parse_headers(headers_string, %{})
+
+    %Conn{
+      method: method,
+      path: path,
+      params: params,
+      headers: headers
+    }
+    |> IO.inspect()
   end
+
+  def parse_headers([head | tail], headers) do
+    [header_name, header_value] = String.split(head, ": ")
+    headers = Map.put(headers, header_name, header_value)
+    parse_headers(tail, headers)
+  end
+
+  def parse_headers([], headers), do: headers
 end
