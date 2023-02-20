8f1ea221e055e24754cab4b0be9f0721cae4ddf9
José Juan Reyes Zuñiga <juan@makingdevs.com>
2023-02-20 15:11:22 -0600

    Add DeveloperController

---
+++ lib/traefik/developer_controller.ex
@@ -0,0 +1,30 @@
+defmodule Traefik.DeveloperController do
+  alias Traefik.Conn
+  alias Traefik.Organization
+
+  def index(%Conn{} = conn) do
+    developers =
+      Organization.list_developers()
+      |> Enum.take(5)
+      |> Enum.map(&"<li>#{&1.id} - #{&1.first_name}</li> ")
+      |> Enum.join("\n")
+
+    developers = """
+    <ul>
+    #{developers}
+    </ul>
+    """
+
+    %{conn | response: developers, status: 200}
+  end
+
+  def show(%Conn{} = conn, %{"id" => id}) do
+    developer = Organization.get_developer(id)
+
+    dev_response = """
+    #{developer.id} - #{developer.first_name} - #{developer.last_name} - #{developer.email}
+    """
+
+    %{conn | response: dev_response, status: 200}
+  end
+end
