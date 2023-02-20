3d10631564aa0a5f6e786899593fa0a43208707c
José Juan Reyes Zuñiga <juan@makingdevs.com>
2023-02-20 15:55:21 -0600

    Use templates in controller for response, adds render fun

--- lib/traefik/developer_controller.ex
+++ lib/traefik/developer_controller.ex
@@ -2,28 +2,26 @@ defmodule Traefik.DeveloperController do
   alias Traefik.Conn
   alias Traefik.Organization
 
+  @templates_location Path.expand("../../templates", __DIR__)
+
   def index(%Conn{} = conn) do
-    developers =
-      Organization.list_developers(%{limit: 5, offset: 0})
-      |> Enum.map(&"<li>#{&1.id} - #{&1.first_name}</li> ")
-      |> Enum.join("\n")
+    developers = Organization.list_developers(%{limit: 5, offset: 0})
 
-    developers = """
-    <ul>
-    #{developers}
-    </ul>
-    """
-
-    %{conn | response: developers, status: 200}
+    render(conn, "index.eex", developers: developers)
   end
 
   def show(%Conn{} = conn, %{"id" => id}) do
     developer = Organization.get_developer(id)
 
-    dev_response = """
-    #{developer.id} - #{developer.first_name} - #{developer.last_name} - #{developer.email}
-    """
+    render(conn, "show.eex", developer: developer)
+  end
 
-    %{conn | response: dev_response, status: 200}
+  defp render(conn, template, bindings \\ []) do
+    response =
+      @templates_location
+      |> Path.join(template)
+      |> EEx.eval_file(bindings)
+
+    %{conn | response: response, status: 200}
   end
 end
