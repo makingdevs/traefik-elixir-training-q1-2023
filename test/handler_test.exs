026c79c3f038de7c5f1c020b664794f06a1a5d29
José Juan Reyes Zuñiga <juan@makingdevs.com>
2023-02-22 15:41:20 -0600

    Add the test for get developers in JSON format

--- test/handler_test.exs
+++ test/handler_test.exs
@@ -211,4 +211,27 @@ defmodule Traefik.HandlerTest do
 
            """
   end
+
+  test "GET /api/developer" do
+    request = """
+    GET /api/developer HTTP/1.1
+    Accept: */*
+    Connection: keep-alive
+    User-Agent: telnet
+
+    """
+
+    response = Handler.handle(request)
+
+    assert response == """
+           HTTP/1.1 200 OK
+           Host: some.com
+           User-Agent: telnet
+           Content-Type: application/json
+           Content-Lenght: 1326
+           Accept: */*
+
+           [{\"email\":\"jrubertis0@nytimes.com\",\"first_name\":\"Jerri\",\"gender\":\"Male\",\"id\":1,\"ip_address\":\"206.67.100.126\",\"last_name\":\"Rubertis\"},{\"email\":\"lgepson1@amazon.com\",\"first_name\":\"Lief\",\"gender\":\"Male\",\"id\":2,\"ip_address\":\"235.91.3.49\",\"last_name\":\"Gepson\"},{\"email\":\"vvanhalle2@quantcast.com\",\"first_name\":\"Viki\",\"gender\":\"Female\",\"id\":3,\"ip_address\":\"53.76.94.126\",\"last_name\":\"Van Halle\"},{\"email\":\"mdubose3@ftc.gov\",\"first_name\":\"Maribelle\",\"gender\":\"Female\",\"id\":4,\"ip_address\":\"107.46.58.239\",\"last_name\":\"Dubose\"},{\"email\":\"vklarzynski4@mtv.com\",\"first_name\":\"Vivian\",\"gender\":\"Agender\",\"id\":5,\"ip_address\":\"7.247.165.222\",\"last_name\":\"Klarzynski\"},{\"email\":\"hbedome5@slashdot.org\",\"first_name\":\"Helyn\",\"gender\":\"Genderqueer\",\"id\":6,\"ip_address\":\"24.241.90.151\",\"last_name\":\"Bedome\"},{\"email\":\"lsidney6@technorati.com\",\"first_name\":\"Lyndsay\",\"gender\":\"Female\",\"id\":7,\"ip_address\":\"54.172.132.20\",\"last_name\":\"Sidney\"},{\"email\":\"epoluzzi7@exblog.jp\",\"first_name\":\"Emmye\",\"gender\":\"Female\",\"id\":8,\"ip_address\":\"83.213.206.127\",\"last_name\":\"Poluzzi\"},{\"email\":\"bfitzsymons8@pen.io\",\"first_name\":\"Barn\",\"gender\":\"Male\",\"id\":9,\"ip_address\":\"117.249.1.245\",\"last_name\":\"Fitzsymons\"},{\"email\":\"amuggleston9@livejournal.com\",\"first_name\":\"Adelheid\",\"gender\":\"Female\",\"id\":10,\"ip_address\":\"91.155.84.10\",\"last_name\":\"Muggleston\"}]
+           """
+  end
 end
