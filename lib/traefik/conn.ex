5905f12df9c484bb7ee7609b37dda882789bc558
José Juan Reyes Zuñiga <juan@makingdevs.com>
2023-02-22 15:42:27 -0600

    Add the content type field for handle html and json responses

--- lib/traefik/conn.ex
+++ lib/traefik/conn.ex
@@ -1,5 +1,11 @@
 defmodule Traefik.Conn do
-  defstruct method: "", path: "", response: "", status: nil, params: %{}, headers: %{}
+  defstruct method: "",
+            path: "",
+            response: "",
+            status: nil,
+            params: %{},
+            headers: %{},
+            content_type: "text/html"
 
   def status(%__MODULE__{} = conn) do
     "#{conn.status} #{status_reason(conn.status)}"
