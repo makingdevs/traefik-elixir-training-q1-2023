c72d7a6feabb8b226ee15e0f243802c75366f214
José Juan Reyes Zuñiga <juan@makingdevs.com>
2023-02-22 14:23:22 -0600

    Test for parse the params

--- test/parser_test.exs
+++ test/parser_test.exs
@@ -7,4 +7,10 @@ defmodule Traefik.ParserTest do
     headers = Traefik.Parser.parse_headers(headers_string, %{})
     assert headers == %{"Accept" => "*/*", "Connection" => "keep-alive"}
   end
+
+  test "parses the params from a request into a map" do
+    params_string = "a=1&b=2&c=3"
+    params = Traefik.Parser.parse_params("application/x-www-form-urlencoded", params_string)
+    assert params == %{"a" => "1", "b" => "2", "c" => "3"}
+  end
 end
