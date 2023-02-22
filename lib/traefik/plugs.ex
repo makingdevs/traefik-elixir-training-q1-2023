31b7ee31bb8bedcd9763fd9a2c7a5a71bd21b21c
José Juan Reyes Zuñiga <juan@makingdevs.com>
2023-02-22 15:15:24 -0600

    Handle environment for plugs to show logs and tracks

--- lib/traefik/plugs.ex
+++ lib/traefik/plugs.ex
@@ -1,6 +1,10 @@
 defmodule Traefik.Plugs do
   alias Traefik.Conn
 
+  @moduledoc """
+  Some functions for extra information about requests
+  """
+
   def rewrite_path(%Conn{path: "/redirectme"} = conn) do
     %{conn | path: "/all"}
   end
@@ -8,14 +12,23 @@ defmodule Traefik.Plugs do
   def rewrite_path(%Conn{} = conn), do: conn
 
   def log(%Conn{} = conn) do
-    # IO.inspect(conn, label: "Logger")
+    show_log(conn, Mix.env())
     conn
   end
 
+  @doc """
+  Shows the request when the env is active
+  """
   def track(%Conn{status: 404, path: _path} = conn) do
-    # IO.inspect("Warn ✊ path #{path} not found")
+    show_track(conn, Mix.env())
     conn
   end
 
   def track(%Conn{} = conn), do: conn
+
+  defp show_log(conn, :dev), do: IO.inspect(conn)
+  defp show_log(_conn, _), do: :ok
+
+  defp show_track(_conn, :test), do: :ok
+  defp show_track(conn, _), do: IO.inspect(conn)
 end
