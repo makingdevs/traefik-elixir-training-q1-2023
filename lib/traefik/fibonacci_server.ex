bd51d3c8a691af158798d026525b7831b119d8ec
José Juan Reyes Zuñiga <juan@makingdevs.com>
2023-02-28 15:14:37 -0600

    Add a fn for handle the message in fibonacci server

--- lib/traefik/fibonacci_server.ex
+++ lib/traefik/fibonacci_server.ex
@@ -9,30 +9,8 @@ defmodule Traefik.FibonacciServer do
     GenericServer.start(__MODULE__)
   end
 
-  # def loop(state) do
-  #   receive do
-  #     {caller, :status} ->
-  #       send(caller, state)
-  #       loop(state)
-
-  #     {caller, n} when is_pid(caller) and is_number(n) ->
-  #       result =
-  #         case Map.get(state, n) do
-  #           nil -> Fibonacci.sequence(n)
-  #           r -> r
-  #         end
-
-  #       send(caller, {:ok, n, result})
-
-  #       state
-  #       |> Map.put_new(n, result)
-  #       |> loop()
-
-  #     :kill ->
-  #       :killed
-
-  #     _ ->
-  #       loop(state)
-  #   end
-  # end
+  def handle_message({:compute, n}, state) do
+    result = Fibonacci.sequence(n)
+    {:ok, result, state}
+  end
 end
