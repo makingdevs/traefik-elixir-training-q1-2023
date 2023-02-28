ba355a45eb431bca8974b7a11b7e3e750cb06f66
José Juan Reyes Zuñiga <juan@makingdevs.com>
2023-02-28 14:22:54 -0600

    Explain the simple server, ephimeral processes

--- lib/traefik/simple_server.ex
+++ lib/traefik/simple_server.ex
@@ -1,5 +1,15 @@
 defmodule Traefik.SimpleServer do
-  def start(socket) do
-    spawn(Traefik.HttpServer, :serve, [socket])
+  def start(request) do
+    parent = self()
+    pid = spawn(__MODULE__, :loop, [parent])
+    send(pid, {parent, :call, request})
+  end
+
+  def loop(pid) do
+    receive do
+      {^pid, :call, request} ->
+        Traefik.Handler.handle(request)
+        |> IO.inspect(label: "RESPONSE")
+    end
   end
 end
