23ef6f52148360fc7d1fd268849b9e0f5ca21c49
José Juan Reyes Zuñiga <juan@makingdevs.com>
2023-02-28 14:52:26 -0600

    Stores the result in the status as a map

--- lib/traefik/fibonacci_server.ex
+++ lib/traefik/fibonacci_server.ex
@@ -5,20 +5,26 @@ defmodule Traefik.FibonacciServer do
   This is the module for SERVER
   """
   def start() do
-    spawn(__MODULE__, :loop, [])
+    spawn(__MODULE__, :loop, [%{}])
   end
 
-  def loop do
+  def loop(state) do
     receive do
-      {caller, n} when is_pid(caller) ->
-        send(caller, {:ok, Fibonacci.sequence(n)})
-        loop()
+      {caller, :status} ->
+        send(caller, state)
+        loop(state)
+
+      {caller, n} when is_pid(caller) and is_number(n) ->
+        result = Fibonacci.sequence(n)
+        send(caller, {:ok, n, result})
+        state = Map.put(state, n, result)
+        loop(state)
 
       :kill ->
         :killed
 
       _ ->
-        loop()
+        loop(state)
     end
   end
 end
