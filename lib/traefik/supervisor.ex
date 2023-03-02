381a8705620b2b04448da04c91a7b118238c7a75
José Juan Reyes Zuñiga <juan@makingdevs.com>
2023-03-02 15:49:57 -0600

    Add start_link to Supervisor

--- lib/traefik/supervisor.ex
+++ lib/traefik/supervisor.ex
@@ -1,6 +1,10 @@
 defmodule Traefik.Supervisor do
   use Supervisor
 
+  def start_link(opts) do
+    Supervisor.start_link(__MODULE__, opts, name: __MODULE__)
+  end
+
   def init(_) do
     children = [
       Traefik.FibonacciGenServer,
