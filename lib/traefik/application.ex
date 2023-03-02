4339f3e685ab3ce789fe9d437e67d1e72fcd0069
José Juan Reyes Zuñiga <juan@makingdevs.com>
2023-03-02 15:50:09 -0600

    Add Application to Traefik

---
+++ lib/traefik/application.ex
@@ -0,0 +1,8 @@
+defmodule Traefik.Application do
+  use Application
+
+  def start(_type, _args) do
+    children = [Traefik.Supervisor]
+    Supervisor.start_link(children, strategy: :one_for_one)
+  end
+end
--- mix.exs
+++ mix.exs
@@ -14,6 +14,7 @@ defmodule Traefik.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
+      mod: {Traefik.Application, []},
       extra_applications: [:logger, :eex]
     ]
   end
