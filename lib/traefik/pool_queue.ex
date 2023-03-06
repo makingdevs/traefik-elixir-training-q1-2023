0c564fdbc2bb4eafbc72237531ad0b09dce8af88
José Juan Reyes Zuñiga <juan@makingdevs.com>
2023-03-06 14:32:46 -0600

    Add callbacks for Queue

--- lib/traefik/pool_queue.ex
+++ lib/traefik/pool_queue.ex
@@ -5,7 +5,8 @@ defmodule Traefik.PoolQueue do
     GenServer.start_link(__MODULE__, [], name: __MODULE__)
   end
 
-  def init([]) do
-    {:ok, []}
-  end
+  def init([]), do: {:ok, []}
+  def handle_call(:get, _from, queue), do: {:reply, {:ok, queue}, queue}
+  def handle_call(:get_pid, _from, [pid | queue]), do: {:reply, {:ok, pid}, queue ++ [pid]}
+  def handle_cast({:in, pid}, queue), do: {:noreply, queue ++ [pid]}
 end
