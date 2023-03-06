82f3f0bfb0de76016e94705cb30e7e3c4f234e20
José Juan Reyes Zuñiga <juan@makingdevs.com>
2023-03-06 15:23:27 -0600

    Add start_link with mod and workers, and setup the state in init fn

--- lib/traefik/pool_queue.ex
+++ lib/traefik/pool_queue.ex
@@ -1,8 +1,20 @@
 defmodule Traefik.PoolQueue do
   use GenServer
 
-  def start_link([]) do
-    GenServer.start_link(__MODULE__, [], name: __MODULE__)
+  def start_link(worker: {mod, fun, args}, n_workers: n_workers, name: name) do
+    GenServer.start_link(__MODULE__, [{mod, fun, args}, n_workers], name: name)
+  end
+
+  def init([{mod, fun, args}, n_workers]) do
+    queue =
+      1..n_workers
+      |> Enum.to_list()
+      |> Enum.map(fn _ ->
+        {:ok, pid} = :erlang.apply(mod, fun, [args])
+        pid
+      end)
+
+    {:ok, queue}
   end
 
   def exec_compute(n) do
