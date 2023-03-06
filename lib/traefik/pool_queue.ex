77494868ef18c2d421edda7a68524ed7156a8ef0
José Juan Reyes Zuñiga <juan@makingdevs.com>
2023-03-06 15:46:45 -0600

    Add monitor to process, adjust handles

--- lib/traefik/pool_queue.ex
+++ lib/traefik/pool_queue.ex
@@ -5,33 +5,46 @@ defmodule Traefik.PoolQueue do
     GenServer.start_link(__MODULE__, [{mod, fun, args}, n_workers], name: name)
   end
 
-  def init([{mod, fun, args}, n_workers]) do
-    queue =
-      1..n_workers
-      |> Enum.to_list()
-      |> Enum.map(fn _ ->
-        {:ok, pid} = :erlang.apply(mod, fun, [args])
-        pid
-      end)
-
-    {:ok, queue}
-  end
-
-  def exec_compute(n) do
-    {:ok, pid} = GenServer.call(__MODULE__, :get_pid)
+  def exec_compute(name, n) do
+    {:ok, pid} = GenServer.call(name, :get_pid)
     GenServer.call(pid, {:compute, n})
   end
 
-  def get, do: GenServer.call(__MODULE__, :get)
-  def get_pid, do: GenServer.call(__MODULE__, :get_pid)
+  def get(name), do: GenServer.call(name, __MODULE__, :get)
+  def get_pid(name), do: GenServer.call(name, __MODULE__, :get_pid)
 
   def add_pid(worker_module) do
     {:ok, pid} = :erlang.apply(worker_module, :start_link, [[]])
     GenServer.cast(__MODULE__, {:in, pid})
   end
 
-  def init([]), do: {:ok, []}
+  def init([{mod, fun, args}, n_workers]) do
+    Process.flag(:trap_exit, true)
+
+    queue =
+      1..n_workers
+      |> Enum.to_list()
+      |> Enum.map(fn _ ->
+        {:ok, pid} = :erlang.apply(mod, fun, [args])
+        ref = :erlang.monitor(:process, pid)
+        %{ref: ref, pid: pid}
+      end)
+
+    {:ok, queue}
+  end
+
   def handle_call(:get, _from, queue), do: {:reply, {:ok, queue}, queue}
-  def handle_call(:get_pid, _from, [pid | queue]), do: {:reply, {:ok, pid}, queue ++ [pid]}
-  def handle_cast({:in, pid}, queue), do: {:noreply, queue ++ [pid]}
+
+  def handle_call(:get_pid, _from, [%{pid: pid} = pid_ref | queue]),
+    do: {:reply, {:ok, pid}, queue ++ [pid_ref]}
+
+  def handle_cast({:in, pid}, queue) do
+    ref = :erlang.monitor(:process, pid)
+    {:noreply, queue ++ [%{ref: ref, pid: pid}]}
+  end
+
+  def handle_info(msg, state) do
+    IO.inspect(binding())
+    {:noreply, state}
+  end
 end
