20c4e9768f7884b4f9d72cacc16446184d2943da
José Juan Reyes Zuñiga <juan@makingdevs.com>
2023-03-06 14:38:27 -0600

    Add simple funs for GenServer.calls!

--- lib/traefik/pool_queue.ex
+++ lib/traefik/pool_queue.ex
@@ -5,6 +5,10 @@ defmodule Traefik.PoolQueue do
     GenServer.start_link(__MODULE__, [], name: __MODULE__)
   end
 
+  def get, do: GenServer.call(__MODULE__, :get)
+  def get_pid, do: GenServer.call(__MODULE__, :get_pid)
+  def add_pid(pid), do: GenServer.cast(__MODULE__, {:in, pid})
+
   def init([]), do: {:ok, []}
   def handle_call(:get, _from, queue), do: {:reply, {:ok, queue}, queue}
   def handle_call(:get_pid, _from, [pid | queue]), do: {:reply, {:ok, pid}, queue ++ [pid]}
